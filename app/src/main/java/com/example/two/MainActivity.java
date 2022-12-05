b40fa38df4e7614af26c92795f3b637478dc480a
Shaked <sa5426@bs.amalnet.k12.il>
2022-12-05 11:34:50 +0200

    initial commit

---
+++ app/src/main/java/com/example/two/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.two;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+    }
+
+    public void clicked(View view) {
+        count++;
+        btn.setText("Clicks: "+count);
+    }
+}
